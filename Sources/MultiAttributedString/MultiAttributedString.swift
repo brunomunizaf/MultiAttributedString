3881cf97ba55302336bcb8f32c324b3ed30e392e
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2023-10-26 01:43:06 -0300

    Improved API

--- Sources/MultiAttributedString/MultiAttributedString.swift
+++ Sources/MultiAttributedString/MultiAttributedString.swift
@@ -1,90 +1,78 @@
 import UIKit
 
-/// Represents a combination of text attributes and a unique symbol to apply those attributes.
-/// The symbol acts as a delimiter, indicating where to start and stop applying the given attributes.
-public struct AttributeSymbolPair {
-
-  /// The set of attributes to be applied to the text enclosed by the associated symbol.
-  let attributes: [NSAttributedString.Key: Any]
-
-  /// The unique symbol used as a delimiter to signal where the attributes should be applied.
-  let symbol: String
-}
-
 public extension NSAttributedString {
 
-  /// Creates an attributed string by applying the specified attributes between given symbol pairs.
+  /// Creates an `NSAttributedString` by applying the specified attributes between given symbol pairs.
   ///
-  /// This method allows for easy creation of attributed strings using simple symbol delimiters
-  /// to specify where certain attributes should be applied. For instance, if you wanted to make
-  /// text between '$' symbols red, you would include an `AttributeSymbolPair` with the '$' symbol
-  /// and a red text color attribute.
+  /// This method allows you to easily stylize parts of a string using symbol delimiters. For example,
+  /// to make the text between '$' symbols red, you would include an attribute with the '$' symbol and a red text color.
   ///
   /// - Parameters:
-  ///   - rawText: The original string containing symbol delimiters.
-  ///   - pairs: An array of `AttributeSymbolPair` objects defining which attributes to apply and where.
-  /// - Returns: An `NSAttributedString` with the specified attributes applied.
+  ///   - rawText: The input string containing symbol delimiters to denote areas to be stylized.
+  ///   - pairs: A dictionary where each key is a symbol delimiter and the corresponding value is a set of attributes to apply.
+  /// - Returns: An `NSAttributedString` with the attributes applied based on the symbol delimiters.
   static func with(
     _ rawText: String,
-    attributesBetween pairs: [AttributeSymbolPair]
+    attributesBetween pairs: [String: ([NSAttributedString.Key: Any])]
   ) -> NSAttributedString {
 
-    // Replace any escaped symbols with a placeholder to ensure they are not processed.
+    // Replace any escaped symbols (e.g., \*) with a placeholder to prevent them from being processed.
     var preprocessedText = rawText
     for pair in pairs {
-      let escapedSymbol = "\\\(pair.symbol)"
-      let placeholder = "{\(pair.symbol)}" // Placeholder to replace escaped symbols.
+      let escapedSymbol = "\\\(pair.key)"
+      let placeholder = "{\(pair.key)}" // Placeholder to replace escaped symbols.
       preprocessedText = preprocessedText.replacingOccurrences(of: escapedSymbol, with: placeholder)
     }
 
     let mutableAttributedString = NSMutableAttributedString(string: preprocessedText)
 
-    // Stacks to manage the opening and closing of symbols.
+    // Use stacks to track the positions of opening and closing symbols.
     var symbolStack: [String] = []
     var startPositionStack: [String.Index] = []
 
     var currentIndex = preprocessedText.startIndex
 
-    // Process each character in the string.
+    // Iterate through the string's characters.
     while currentIndex < preprocessedText.endIndex {
       let character = preprocessedText[currentIndex]
       let symbol = String(character)
 
-      // Check if the current character matches any of the symbols from the pairs.
-      if let pair = pairs.first(where: { $0.symbol == symbol }) {
-        if symbolStack.contains(pair.symbol) {
-          if let lastSymbol = symbolStack.last, lastSymbol == pair.symbol {
+      // If the current character matches a symbol from the pairs...
+      if let pair = pairs.first(where: { $0.key == symbol }) {
+        if symbolStack.contains(pair.key) {
+          // If it's a closing symbol, pop from the stacks and apply the attributes.
+          if let lastSymbol = symbolStack.last, lastSymbol == pair.key {
             let start = startPositionStack.removeLast()
             let range = NSRange(start..<currentIndex, in: preprocessedText)
-            mutableAttributedString.addAttributes(pair.attributes, range: range)
+            mutableAttributedString.addAttributes(pair.value, range: range)
             symbolStack.removeLast()
           }
         } else {
-          // If it's an opening symbol, push to the stacks.
-          symbolStack.append(pair.symbol)
+          // If it's an opening symbol, push its details to the stacks.
+          symbolStack.append(pair.key)
           startPositionStack.append(currentIndex)
         }
       }
-
+      
       currentIndex = preprocessedText.index(after: currentIndex)
     }
 
-    // Remove the symbols from the final string.
+    // Remove the delimiter symbols from the final string.
     for pair in pairs {
       mutableAttributedString.mutableString.replaceOccurrences(
-        of: pair.symbol,
+        of: pair.key,
         with: "",
         options: [],
         range: NSRange(location: 0, length: mutableAttributedString.length)
       )
     }
 
-    // Replace placeholders with their original symbols.
+    // Replace placeholders with their original symbols (e.g., replace "{*}" with "*").
     for pair in pairs {
-      let placeholder = "{\(pair.symbol)}"
+      let placeholder = "{\(pair.key)}"
       mutableAttributedString.mutableString.replaceOccurrences(
         of: placeholder,
-        with: pair.symbol,
+        with: pair.key,
         options: [],
         range: NSRange(location: 0, length: mutableAttributedString.length)
       )
--- Tests/MultiAttributedStringTests/MultiAttributedStringTests.swift
+++ Tests/MultiAttributedStringTests/MultiAttributedStringTests.swift
@@ -2,42 +2,22 @@ import XCTest
 @testable import MultiAttributedString
 
 final class MultiAttributedStringTests: XCTestCase {
-  var pairs: [AttributeSymbolPair]!
-
-  override func tearDown() {
-    pairs = nil
-    super.tearDown()
-  }
-
-  override func setUp() {
-    super.setUp()
-
-    let attachment = NSTextAttachment()
-    attachment.image = UIImage(named: "sampleImage")
-    attachment.bounds = CGRect(x: 0, y: 0, width: 50, height: 50)
-
-    pairs = [
-      AttributeSymbolPair(attributes: [.foregroundColor: UIColor.red], symbol: "$"),
-      AttributeSymbolPair(attributes: [.foregroundColor: UIColor.blue], symbol: "#"),
-      AttributeSymbolPair(attributes: [.foregroundColor: UIColor.purple], symbol: "@"),
-      AttributeSymbolPair(attributes: [.link: URL(string: "https://www.example.com")!], symbol: "^"),
-      AttributeSymbolPair(attributes: [.attachment: attachment], symbol: "*")
-    ]
-  }
-
-  // This test checks the basic functionality of the extension by ensuring that text between `$` symbols is colored red.
+  // Test attribute application between `$` symbols.
   func testSimpleAttribution() {
-    let result = NSAttributedString.with("This is $red$ text.", attributesBetween: pairs)
+    let result = NSAttributedString.with("This is $red$ text.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red]
+    ])
     let range = (result.string as NSString).range(of: "red")
     let attributes = result.attributes(at: range.location, effectiveRange: nil)
 
     XCTAssertEqual(attributes[.foregroundColor] as! UIColor, UIColor.red)
   }
 
-  // This test ensures that symbols preceded by a backslash (`\`) are treated as literals and not as attribute delimiters.
+  // Test escaping of symbols using a backslash.
   func testEscapeCharacter() {
-    let result = NSAttributedString.with("This is \\$not red\\$ text but this is $red$.", attributesBetween: pairs)
-
+    let result = NSAttributedString.with("This is \\$not red\\$ text but this is $red$.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red]
+    ])
     let rangeOfNotRed = (result.string as NSString).range(of: "not red")
     let attributesOfNotRed = result.attributes(at: rangeOfNotRed.location, effectiveRange: nil)
     XCTAssertNil(attributesOfNotRed[.foregroundColor])
@@ -47,10 +27,12 @@ final class MultiAttributedStringTests: XCTestCase {
     XCTAssertEqual(attributesOfRed[.foregroundColor] as! UIColor, UIColor.red)
   }
 
-  // This test checks that multiple attribute pairs can be applied correctly within the same string.
+  // Test application of multiple attributes.
   func testMultipleAttributions() {
-    let result = NSAttributedString.with("This is $red$ and this is #blue#.", attributesBetween: pairs)
-
+    let result = NSAttributedString.with("This is $red$ and this is #blue#.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red],
+      "#": [.foregroundColor: UIColor.blue]
+    ])
     let rangeOfRed = (result.string as NSString).range(of: "red")
     let attributesOfRed = result.attributes(at: rangeOfRed.location, effectiveRange: nil)
     XCTAssertEqual(attributesOfRed[.foregroundColor] as! UIColor, UIColor.red)
@@ -60,23 +42,23 @@ final class MultiAttributedStringTests: XCTestCase {
     XCTAssertEqual(attributesOfBlue[.foregroundColor] as! UIColor, UIColor.blue)
   }
 
-  // This test evaluates how nested symbols are handled, ensuring that the innermost symbols are processed first.
+  // Test nested symbols' attribute application.
   func testNestedSymbols() {
-    let result = NSAttributedString.with("This is a $#nested$# test.", attributesBetween: pairs)
-
+    let result = NSAttributedString.with("This is a $#nested$# test.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red],
+      "#": [.foregroundColor: UIColor.blue]
+    ])
     let rangeOfNested = (result.string as NSString).range(of: "nested")
     let attributesOfNested = result.attributes(at: rangeOfNested.location, effectiveRange: nil)
     XCTAssertEqual(attributesOfNested[.foregroundColor] as! UIColor, UIColor.red)
-
-    let rangeOfFullNestedText = (result.string as NSString).range(of: "nested", options: .backwards)
-    let attributesOfFullNestedText = result.attributes(at: rangeOfFullNestedText.location, effectiveRange: nil)
-    XCTAssertEqual(attributesOfFullNestedText[.foregroundColor] as! UIColor, UIColor.blue)
   }
 
-  // This test checks how consecutive different attributes are applied without overlapping.
+  // Test consecutive non-overlapping attributes.
   func testConsecutiveDifferentAttributes() {
-    let result = NSAttributedString.with("This is $red$#blue# text.", attributesBetween: pairs)
-
+    let result = NSAttributedString.with("This is $red$#blue# text.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red],
+      "#": [.foregroundColor: UIColor.blue]
+    ])
     let rangeOfRed = (result.string as NSString).range(of: "red")
     let attributesOfRed = result.attributes(at: rangeOfRed.location, effectiveRange: nil)
     XCTAssertEqual(attributesOfRed[.foregroundColor] as! UIColor, UIColor.red)
@@ -86,32 +68,22 @@ final class MultiAttributedStringTests: XCTestCase {
     XCTAssertEqual(attributesOfBlue[.foregroundColor] as! UIColor, UIColor.blue)
   }
 
-  // This test ensures that unpaired symbols are ignored and don't apply any attributes.
+  // Test that unpaired symbols don't apply attributes.
   func testUnpairedSymbols() {
-    let result = NSAttributedString.with("This is $unpaired text.", attributesBetween: pairs)
-
+    let result = NSAttributedString.with("This is $unpaired text.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red]
+    ])
     let rangeOfUnpaired = (result.string as NSString).range(of: "unpaired")
     let attributesOfUnpaired = result.attributes(at: rangeOfUnpaired.location, effectiveRange: nil)
     XCTAssertNil(attributesOfUnpaired[.foregroundColor])
   }
 
-  // This test checks how adjacent symbols are processed, ensuring that each symbol pair applies its respective attributes correctly.
-  func testAdjacentSymbols() {
-    let result = NSAttributedString.with("This is $red$#blue# text.", attributesBetween: pairs)
-
-    let rangeOfRed = (result.string as NSString).range(of: "red")
-    let attributesOfRed = result.attributes(at: rangeOfRed.location, effectiveRange: nil)
-    XCTAssertEqual(attributesOfRed[.foregroundColor] as! UIColor, UIColor.red)
-
-    let rangeOfBlue = (result.string as NSString).range(of: "blue")
-    let attributesOfBlue = result.attributes(at: rangeOfBlue.location, effectiveRange: nil)
-    XCTAssertEqual(attributesOfBlue[.foregroundColor] as! UIColor, UIColor.blue)
-  }
-
-  // This test ensures that multiple escaped symbols within a string are correctly treated as literals.
+  // Test handling of multiple escaped symbols.
   func testMultipleEscapedSymbols() {
-    let result = NSAttributedString.with("This is \\$not\\$ \\#colored\\# text.", attributesBetween: pairs)
-
+    let result = NSAttributedString.with("This is \\$not\\$ \\#colored\\# text.", attributesBetween: [
+      "$": [.foregroundColor: UIColor.red],
+      "#": [.foregroundColor: UIColor.blue]
+    ])
     let rangeOfNot = (result.string as NSString).range(of: "not")
     let attributesOfNot = result.attributes(at: rangeOfNot.location, effectiveRange: nil)
     XCTAssertNil(attributesOfNot[.foregroundColor])
@@ -121,4 +93,3 @@ final class MultiAttributedStringTests: XCTestCase {
     XCTAssertNil(attributesOfColored[.foregroundColor])
   }
 }
-
